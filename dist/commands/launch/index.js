fd1d14002cba154287b9b1f40a24e677dd87c6fd
Jaron Zijlstra <support@jaronline.dev>
2025-06-07 18:14:27 +0200

    chore: rename options to parsers

--- dist/commands/launch/index.js
+++ dist/commands/launch/index.js
@@ -16,7 +16,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 const { Option } = require("commander");
-const { parseSide, parseEnvName } = require("../../options");
+const { parseSide, parseEnvName } = require("../../parsers");
 
 /**
  * @param {import("../../lib").Program} program
