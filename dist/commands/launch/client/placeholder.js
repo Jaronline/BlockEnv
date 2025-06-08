4648743f66645339129cf2d00a4e92c5550d9caa
Alper Bekar <117603269+A-Bekar@users.noreply.github.com>
2025-06-08 17:16:29 +0200

    Update dist/commands/launch/client/placeholder.js
    Co-authored-by: Jaron Zijlstra <support@jaronline.dev>
    Signed-off-by: Alper Bekar <117603269+A-Bekar@users.noreply.github.com>

--- dist/commands/launch/client/placeholder.js
+++ dist/commands/launch/client/placeholder.js
@@ -16,8 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-
-
 module.exports.subsitutePlaceholders = function(text,
     {
         version, classpath, loaderVersion, libDir, classpathSeparator,
