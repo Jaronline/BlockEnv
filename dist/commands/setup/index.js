c7052795eee8bb399e3e5a9c577676bcd044674c
Jaron Zijlstra <support@jaronline.dev>
2025-06-15 13:48:41 +0200

    fix: side parser missing (#20)

--- dist/commands/setup/index.js
+++ dist/commands/setup/index.js
@@ -15,7 +15,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-const { parseSide, parseLoader, parseEnvName } = require("../../parsers");
+const { parseLoader, parseEnvName } = require("../../parsers");
 const { Option } = require("commander");
 
 /**
--- dist/parsers/index.js
+++ dist/parsers/index.js
@@ -17,6 +17,5 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 module.exports = {
     parseLoader: require("./loader").parseLoader,
-    parseSide: require("./side").parseSide,
     ...require("./env"),
 };
