d6cd933c8cdf22014a831add15ad7565d25ccdb2
Jaron Zijlstra <support@jaronline.dev>
2025-07-21 17:07:14 +0200

    feat: configure docusaurus

--- apps/website/docusaurus.config.ts
+++ apps/website/docusaurus.config.ts
@@ -2,147 +2,90 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+const Title = 'BlockEnv';
+const Description = 'Minecraft Java testing environment for modpacks';
+const Email = 'info@jaronline.dev';
+const BaseUrl = 'https://jaronline.github.io';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
-
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
-  future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
-  },
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
-
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
-        },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+	title: Title,
+	tagline: Description,
+	favicon: 'img/favicon.ico',
+	future: {
+		v4: true,
+	},
+	url: BaseUrl,
+	baseUrl: '/BlockEnv/',
+	organizationName: 'Jaronline',
+	projectName: 'BlockEnv',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
+	presets: [
+		[
+			'classic',
+			{
+				docs: {
+					sidebarPath: './sidebars.ts',
+					editUrl:
+					  'https://github.com/Jaronline/BlockEnv/edit/main/apps/website/',
+				},
+				blog: false,
+				theme: {
+					customCss: './src/css/custom.css',
+				},
+			} satisfies Preset.Options,
+		],
+	],
+	themeConfig: {
+		// Replace with your project's social card
+		image: 'img/docusaurus-social-card.jpg',
+		colorMode: {
+			defaultMode: 'dark',
+			respectPrefersColorScheme: true
+		},
+		metadata: [
+			{ name: 'application-name', content: Title },
+			{ name: 'author', content: `Jaronline, ${Email}` },
+			{ name: 'description', content: Description },
+			{ name: 'url', content: BaseUrl },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ property: 'og:description', content: Description },
+			{ property: 'og:email', content: Email },
+			{ property: 'og:locale', content: "en_US" },
+			{ property: 'og:site_name', content: Title },
+			{ property: 'og:title', content: Title },
+			{ property: 'og:type', content: "website" },
+			{ property: 'og:url', content: BaseUrl }
+		],
+		navbar: {
+			title: Title,
+			logo: {
+				alt: `${Title} Logo`,
+				src: 'img/logo.svg',
+			},
+			items: [
+				{
+					href: 'https://github.com/Jaronline/BlockEnv',
+					label: 'GitHub',
+					position: 'right',
+				},
+			],
+		},
+		footer: {
+			style: 'dark',
+			links: [],
+			copyright: `Copyright © ${new Date().getFullYear()} Jaronline.`,
+		},
+		prism: {
+			defaultLanguage: 'bash',
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 };
 
 export default config;
