0a66c7863167219fbf1c48cb63d4311be4b2c16f
Jaron Zijlstra <support@jaronline.dev>
2025-08-22 12:57:24 +0200

    docs: setup command (#67)
    Co-authored-by: Alper Bekar <alperbekar05@gmail.com>

--- scripts/generate-cli-docs.mjs
+++ scripts/generate-cli-docs.mjs
@@ -1,7 +1,7 @@
 import createProgram from "../packages/blockenv/dist/index.js";
 import { existsSync } from "node:fs";
 import { mkdir, writeFile } from "node:fs/promises";
-import stripAnsi from "strip-ansi";
+import { Command } from "commander";
 
 const rootDir = new URL("../", import.meta.url);
 const outputDir = new URL("./apps/website/docs/commands/_generated_help/", rootDir);
@@ -15,12 +15,9 @@ const rootProgram = createProgram();
 const results = await Promise.all(
     [rootProgram, ...rootProgram.commands].map(async command => {
         try {
-            const help = command.createHelp();
-            const output = help.formatHelp(command, help);
+            const output = customFormat(command);
             const destURL = new URL(`./${command.name()}.md`, outputDir);
-            const content = stripAnsi(output.trim());
-            const mdContent = "```bash\n" + content + "\n```";
-            await writeFile(destURL, mdContent, { encoding: "utf-8" });
+            await writeFile(destURL, output, { encoding: "utf-8" });
             return { command: command.name(), success: true };
         } catch (err) {
             console.error(`Error processing command '${command.name()}':`, err);
@@ -28,6 +25,44 @@ const results = await Promise.all(
         }
     })
 );
+
+/**
+ *
+ * @param { Command } command
+ * @returns {string}
+ */
+function customFormat(command) {
+    const help = command.createHelp();
+
+    let md = `## Usage \n\n\`\`\`\n${help.commandUsage(command)}\n\`\`\`\n\n${help.commandDescription(command)}\n\n`;
+
+    const optionGroups = help.groupItems(command.options, help.visibleOptions(command), (option) => option.helpGroupHeading ?? "Options:");
+    if (optionGroups.size > 0) {
+        optionGroups.forEach((options, group) => {
+            md += `### ${group.replace(/(.*):/, "$1")}\n\n`;
+            md += "| Option | Description |\n";
+            md += "|--------|-------------|\n";
+            md += options.map( (option) => {
+                return `| ${help.optionTerm(option)} | ${help.optionDescription(option)} |`
+            }).join("\n") + "\n\n";
+        });
+    }
+
+    const commandGroups = help.groupItems(command.commands, help.visibleCommands(command), (sub) => sub.helpGroup() || "Commands:");
+    if (commandGroups.size > 0) {
+        commandGroups.forEach((commands,group) => {
+            md += `### ${group.replace(/(.*):/, "$1")}\n\n`;
+            md += "| Sub Command | Description |\n";
+            md += "|--------|-----------------|\n";
+            md += commands.map( (sub) => {
+                return `| ${help.subcommandTerm(sub)} | ${help.subcommandDescription(sub)} |`
+            }).join("\n") + "\n\n";
+        });
+    }
+
+    return md.replaceAll("<","&lt;").replaceAll(">","&gt;");
+
+}
 
 // Optionally, check for errors and exit with non-zero code if any failed
 const failed = results.filter(r => !r.success);
